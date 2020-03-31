9e23f5b8381949ab4dafd4a80c5f3802af4f2efe
zhu.hao <2585619006@qq.com>
2020-03-31 14:47:50 +0800

    update

--- chzu-jc-service/src/main/java/cn/chzu/buildingmaterials/priority/web/PriorityController.java
+++ chzu-jc-service/src/main/java/cn/chzu/buildingmaterials/priority/web/PriorityController.java
@@ -1,8 +1,15 @@
 package cn.chzu.buildingmaterials.priority.web;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.chzu.buildingmaterials.priority.model.Priority;
+import cn.chzu.buildingmaterials.priority.service.PriorityService;
+import cn.chzu.buildingmaterials.storegoods.model.Store;
+import cn.chzu.conf.common.ReqObject;
+import cn.chzu.conf.common.ResObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @description: 微信小程序商品推广管理
@@ -16,4 +23,28 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/priority")
 public class PriorityController {
+
+    @Autowired
+    PriorityService priorityService;
+
+    /**
+     * @param request
+     * @param response
+     * @param data
+     * @return cn.chzu.conf.common.ResObject<cn.chzu.buildingmaterials.priority.model.Priority>
+     * @Title updatePriority
+     * @description 首页优先级推广管理
+     * @author zhu_hao
+     * @date 2020/3/31 14:47
+     */
+    @RequestMapping(value = "/updatePriority", method = RequestMethod.POST)
+    @ResponseBody
+    public ResObject<Priority> updatePriority(HttpServletRequest request, HttpServletResponse response, @RequestBody ReqObject<Store> data) {
+
+
+        String id = data.getObject().getId();
+        String priority = data.getObject().getPriority();
+        Priority updatePriority = priorityService.updatePriority(id, priority);
+        return new ResObject<>(null, updatePriority);
+    }
 }
